24473e3305ec468d4b3f8b2ad6c754155777bd2b
xxrainow <sjw1770@seoultech.ac.kr>
2025-11-28 13:06:16 +0900

    수정

--- webfinalproject/src/App.js
+++ webfinalproject/src/App.js
@@ -1,32 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Recent from './pages/Recent';
-import Navbar from './components/layout/Navbar';
+import logo from './logo.svg';
 import './App.css';
-
-// 다른 페이지들의 더미 컴포넌트 (실제로는 별도 파일로 분리하세요)
-const Request = () => <div className="page-placeholder">Request Page</div>;
-const Map = () => <div className="page-placeholder">Map Page</div>;
-const Note = () => <div className="page-placeholder">Note Page</div>;
-const Setting = () => <div className="page-placeholder">Setting Page</div>;
 
 function App() {
   return (
-    <Router>
-      <div className="app-container">
-        {/* 네비게이션 바 */}
-        <Navbar />
-
-        {/* 페이지 라우팅 */}
-        <Routes>
-          <Route path="/" element={<Recent />} />
-          <Route path="/request" element={<Request />} />
-          <Route path="/map" element={<Map />} />
-          <Route path="/note" element={<Note />} />
-          <Route path="/setting" element={<Setting />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
